4c74067bb326455b5caa471d971f160949604f88
emmanuelughele <osagieughele@gmail.com>
2025-04-13 13:59:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -56,4 +56,8 @@ app.post("/delete/:index", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
